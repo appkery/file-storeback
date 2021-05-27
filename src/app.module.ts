848311dcf510e7521c4e2ac47c28d28c6e97a1a9
appkery <appkery@gmail.com>
2021-05-28 01:52:03 +0900

    app.module 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+import GraphQLJSON from 'graphql-type-json';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RolesModule } from './roles/roles.module';
-import GraphQLJSON from 'graphql-type-json';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
-      resolvers: { JSON: GraphQLJSON }
-      // definitions: {
-      //   path: join(process.cwd(), 'src/graphql.ts'),
-      //   outputAs: 'class'
-      // },
+      resolvers: { Json: GraphQLJSON }
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
@@ -27,17 +19,13 @@ import GraphQLJSON from 'graphql-type-json';
       username: 'admin',
       password: 'Qwer!234',
       database: 'file_storeback',
-      // entities: [
-      //   User
-      // ],
       autoLoadEntities: true,
       synchronize: true,
       keepConnectionAlive: true
     }),
-    UsersModule
-  ],
-  // controllers: [AppController],
-  // providers: [AppService],
+    UsersModule,
+    RolesModule
+  ]
 })
 export class AppModule {
   constructor(private connection: Connection) {}
