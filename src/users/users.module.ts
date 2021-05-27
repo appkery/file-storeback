f53c497351c965779e08f5c0edb1ffce5feae8c1
appkery <appkery@gmail.com>
2021-05-28 01:54:45 +0900

    users.modules 정리

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersResolver } from './users.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 
 @Module({
