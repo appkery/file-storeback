a5337fecc0e4f6ce08f5c38d5ac5b4b7e02be936
terry <terry@gsitm.com>
2021-06-01 19:31:50 +0900

    add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -29,7 +31,9 @@ import { AuthModule } from './auth/auth.module';
     RolesModule,
     OrdersModule,
     AuthModule
-  ]
+  ],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {
   constructor(private connection: Connection) {}
