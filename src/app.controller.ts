d4b820a830439e29b5135b114618014a98900976
terry <terry@gsitm.com>
2021-06-01 19:35:32 +0900

    add guards, strategies folders

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
