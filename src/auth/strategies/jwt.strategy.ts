9b05d7bcf7727f18ca5b05ced6b8299544e934ed
appkery <appkery@gmail.com>
2021-09-27 01:00:27 +0900

    fix JwtStrategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -14,7 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log('payload', payload);
-    return { username: payload.username, sub: payload.sub };
+    return { id: payload.sub, username: payload.username };
   }
 }
