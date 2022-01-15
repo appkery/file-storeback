ab63f9d60826fa6be7b8fe7004e1c46ddb82c9bc
appkery <appkery@gmail.com>
2022-01-16 06:19:56 +0900

    date column로 변경

--- src/crud.entity.ts
+++ src/crud.entity.ts
@@ -1,15 +1,15 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class Crud {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @CreateDateColumn()
   created_at?: Date;
 
-  @Column()
+  @UpdateDateColumn()
   updated_at?: Date;
 
-  @Column()
+  @DeleteDateColumn()
   deleted_at?: Date;
 }
